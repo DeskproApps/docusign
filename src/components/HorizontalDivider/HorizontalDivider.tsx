d74d1e2c5ae4dd308c50cfdefdf167937ff45d55
ITweeno <itweeno@gmail.com>
2023-04-20 16:44:25 +0100

    horizontaldivider

---
+++ src/components/HorizontalDivider/HorizontalDivider.tsx
@@ -0,0 +1,14 @@
+import { HorizontalDivider as HorizontalDividerSDK } from "@deskpro/app-sdk";
+
+export const HorizontalDivider = () => {
+  return (
+    <HorizontalDividerSDK
+      style={{
+        width: "110%",
+        color: "#EFF0F0",
+        marginBottom: "10px",
+        marginLeft: "-10px",
+      }}
+    />
+  );
+};
