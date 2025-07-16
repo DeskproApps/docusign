14ec5799c2accec97ff159ea16ab3e9667297941
OJ Abba <61709964+Nugwxa@users.noreply.github.com>
2025-07-16 11:59:56 +0100

    [Chore] Update theme typings for styled components

--- src/styles.ts
+++ src/styles.ts
@@ -6,7 +6,7 @@ import { DeskproAppTheme } from "@deskpro/app-sdk";
 export const StyledLink = styled(Link)<DeskproAppTheme>`
   all: unset;
   color: ${({ theme, to }) =>
-    to ? theme.colors.cyan100 : theme.colors.black100};
+    to ? theme.colors.cyan100 : theme.colors.grey100};
   text-decoration: none;
   font-weight: 500;
   font-size: ${({ title }) => (title ? "12px" : "11px")};
